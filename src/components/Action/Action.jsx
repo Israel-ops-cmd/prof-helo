68fb6b0cc8b802cf14134d2c56fd3a339b91cdb5
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-28 15:51:49 -0300

    alterando actions

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -3,8 +3,16 @@ import "./Action.css"
 function Action() {
     return (
         <div className="principal">
-            <h2>Transforme o aprendizado do seu filho! 📚✨</h2>
+            <h1>Transforme o aprendizado do seu filho!</h1>
             <h4>Agende uma aula experimental gratuita e veja a diferença!</h4>
+            <a 
+                href="https://wa.me/558491814786?text=Olá!%20Gostaria%20de%20agendar%20uma%20aula%20e%20conhecer%20mais%20sobre%20o%20seu%20trabalho.%20:)"
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+            >
+                <button className="aperty">Agende uma aula experimental!</button>
+            </a>
         </div>
     )
 }
