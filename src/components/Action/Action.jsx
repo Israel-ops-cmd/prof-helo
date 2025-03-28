864d1b2e937c9244cb871697d9b7334859dafb23
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-28 14:38:36 -0300

    fazendo botao virar classe

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -5,7 +5,7 @@ function Action() {
         <div className="t-align al-center sections" id="totally">
             <h2 className="title">Agende sua aula de forma rápida e fácil</h2>
             {/*<h4 id="height">Não perca mais tempo, comece agora a melhorar os resultados escolares do seu filho.</h4>*/}
-            <button id="botao">Agende sua aula!</button>
+            <button className="botao">Agende sua aula!</button>
         </div>
     )
 }
