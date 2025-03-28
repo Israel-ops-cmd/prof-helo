d2b469ffb9ad7de6ab49998d100a5e77125b1cd8
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-28 10:55:51 -0300

    retirando o botão

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -5,14 +5,6 @@ function Action() {
         <div className="t-align al-center sections" id="totally">
             <h2 className="title">Agende sua aula de forma rápida e fácil</h2>
             <h4>Não perca mais tempo, comece agora a melhorar os resultados escolares do seu filho.</h4>
-            <a 
-                href="https://wa.me/558491814786?text=Olá!%20Gostaria%20de%20agendar%20uma%20aula%20e%20conhecer%20mais%20sobre%20o%20seu%20trabalho.%20:)"
-                target="_blank" 
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none" }}
-            >
-                <button className="cta-button bt">Agendar Agora</button>
-            </a>
         </div>
     )
 }
