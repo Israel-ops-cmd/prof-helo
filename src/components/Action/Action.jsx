b97c44f68c815c75991867187b972ef78fc3da8a
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-28 14:53:37 -0300

    testando o action

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -2,12 +2,10 @@ import "./Action.css"
 
 function Action() {
     return (
-        <div className="t-align al-center sections" id="totally">
-            <h2 className="title">Agende sua aula de forma rápida e fácil</h2>
-            {/*<h4 id="height">Não perca mais tempo, comece agora a melhorar os resultados escolares do seu filho.</h4>*/}
-            <button className="botao">Agende sua aula!</button>
+        <div>
+            <h1>teste</h1>
         </div>
     )
 }
 
-export default Action
+export default Action*/}
